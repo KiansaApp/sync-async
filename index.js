361afffe8220a98e51a27671c81cd702623315b4
KiansaApp <ia.rafael.alves@gmail.com>
2023-08-27 01:50:13 -0300

    start sample code.

---
+++ index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+// Leitura assíncrona de arquivo
+fs.readFile('sample.txt', 'utf8', (err, data) => {
+    if (err) {
+      console.error('Erro ao ler o arquivo:', err);
+      return;
+    }
+    console.log('Data lida:', data);
+  });
+  
+  console.log('Esta mensagem pode ser exibida antes da leitura do arquivo.');
